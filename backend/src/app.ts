9a4ad5ab73edcc224c30824ba4861eb25d6ea692
jucaza1 <juan01cz@gmail.com>
2025-04-09 12:11:20 +0200

    feat: connected router with express app, using inmemoryStore impl , also added a simple global error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,12 @@
-import express from 'express';
+import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { UserMemoryStore } from './models/user_memory';
+import { UserService } from './services/user_service';
+import { UserController } from './controllers/user_controller';
+import { CompanyMemoryStore } from './models/company_memory';
+import { CompanyService } from './services/company_service';
+import { CompanyController } from './controllers/company_controller';
+import { createRouter } from './routes/routes_dev';
 
 const app = express();
 const corsOptions = {
@@ -13,5 +20,17 @@ app.use(cors(corsOptions))
 app.get('/health', (_, res) => {
     res.status(200).json({ message: 'Server is OK' })
 })
+const userController = new UserController(new UserService(new UserMemoryStore()))
+const companyController = new CompanyController(new CompanyService(new CompanyMemoryStore()))
+const router = createRouter(userController, companyController)
 
-export default app;
+app.use('/api/v1', router)
+
+// error handler
+const globalErrorHandler: ErrorRequestHandler = (err: Error, _req: Request, res: Response, _next: NextFunction) => {
+    console.error(err)
+    res.status(500).json({ error: err.message })
+}
+app.use(globalErrorHandler)
+
+export default app
