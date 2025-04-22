9249f091f33fc8ba5c79937890e480dd648532d9
jucaza1 <juan01cz@gmail.com>
2025-04-22 10:59:46 +0200

    fix: exception handling for company and user models

--- backend/src/models/company-prisma.ts
+++ backend/src/models/company-prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { CompanyStore } from "./company"
 import { Company, CompanyCreate } from '../types/db'
 import { ResultStore, StoreErrorCode } from '../types/result'
+import { prismaCatchToStoreError } from '../types/exceptions'
 
 export class CompanyPrismaStore implements CompanyStore {
     private client: PrismaClient
@@ -16,12 +17,16 @@ export class CompanyPrismaStore implements CompanyStore {
     }
     getCompany(id: string): ResultStore<Company> {
         let company: Company | undefined
-        this.client.company.findUnique({ where: { id } }).then((result) => {
-            if (!result) {
-                return
-            }
-            company = result
-        })
+        try {
+            this.client.company.findUnique({ where: { id } }).then((result) => {
+                if (!result) {
+                    return
+                }
+                company = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (!company) {
             return { ok: false, err: { code: StoreErrorCode.notFound, msg: "company not found" } }
         }
@@ -29,60 +34,80 @@ export class CompanyPrismaStore implements CompanyStore {
     }
     getCompanies(): ResultStore<Company[]> {
         let companies: Company[] = []
-        this.client.company.findMany().then((result) => {
-            if (!result) {
-                return
-            }
-            companies = result
-        })
+        try {
+            this.client.company.findMany().then((result) => {
+                if (!result) {
+                    return
+                }
+                companies = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         return { ok: true, data: companies }
     }
     createCompany(company: CompanyCreate): ResultStore<Company> {
         // Check if the company already exists
-        let existingCompany : Company | undefined
-        this.client.company.findUnique({ where: { name: company.name } }).then((result) => {
-            if (result) {
-                existingCompany = result
-            }
-        })
+        let existingCompany: Company | undefined
+        try {
+            this.client.company.findUnique({ where: { name: company.name } }).then((result) => {
+                if (result) {
+                    existingCompany = result
+                }
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (existingCompany) {
             return { ok: false, err: { code: StoreErrorCode.unique, msg: "company already exists" } }
         }
         // Create the company
-        let companyResult : Company | undefined
-        this.client.company.create({ data: company }).then((result) => {
-            if (!result) {
-                return
-            }
-            companyResult = result
-        })
+        let companyResult: Company | undefined
+        try {
+            this.client.company.create({ data: company }).then((result) => {
+                if (!result) {
+                    return
+                }
+                companyResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (!companyResult) {
             return { ok: false, err: { code: StoreErrorCode.unknown, msg: "internal server error" } }
         }
         return { ok: true, data: companyResult }
     }
     updateCompany(id: string, company: Partial<Company>): ResultStore<Company> {
-        let companyResult : Company | undefined
-        this.client.company.update({ where: { id }, data: company }).then((result) => {
-            if (!result) {
-                return
-            }
-            companyResult = result
-        })
+        let companyResult: Company | undefined
+        try {
+            this.client.company.update({ where: { id }, data: company }).then((result) => {
+                if (!result) {
+                    return
+                }
+                companyResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (!companyResult) {
             return { ok: false, err: { code: StoreErrorCode.notFound, msg: "company not found" } }
         }
         return { ok: true, data: companyResult }
     }
     deleteCompany(id: string): ResultStore<Company> {
-        let companyResult : Company | undefined
-        this.client.company.delete({ where: { id } }).then((result) => {
-            // Company deleted
-            if (!result) {
-                return
-            }
-            companyResult = result
-        })
+        let companyResult: Company | undefined
+        try {
+            this.client.company.delete({ where: { id } }).then((result) => {
+                // Company deleted
+                if (!result) {
+                    return
+                }
+                companyResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (!companyResult) {
             return { ok: false, err: { code: StoreErrorCode.notFound, msg: "company not found" } }
         }
@@ -90,13 +115,17 @@ export class CompanyPrismaStore implements CompanyStore {
     }
     getCompaniesByUserId(userId: string): ResultStore<Company[]> {
         let companies: Company[] = []
-        this.client.company.findMany({ where: { users: { some: { id: userId } } } })
-            .then((result) => {
-                if (!result) {
-                    return
-                }
-                companies = result
-            })
+        try {
+            this.client.company.findMany({ where: { users: { some: { id: userId } } } })
+                .then((result) => {
+                    if (!result) {
+                        return
+                    }
+                    companies = result
+                })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         return { ok: true, data: companies }
     }
 }
--- backend/src/models/user-prisma.ts
+++ backend/src/models/user-prisma.ts
@@ -1,99 +1,153 @@
 import { PrismaClient } from '@prisma/client'
-// import { UserStore } from "./user"
+import { UserStore } from "./user"
 import { User, UserCreate, UserCreatePrismaConverter } from '../types/db'
+import { ResultStore, StoreErrorCode } from '../types/result'
+import { prismaCatchToStoreError } from '../types/exceptions'
 
-// export class UserPrismaStore implements UserStore {
-export class UserPrismaStore {
+export class UserPrismaStore implements UserStore {
     private client: PrismaClient
     constructor(client: PrismaClient) {
         this.client = client
+        this.getUsers = this.getUsers.bind(this)
+        this.getUser = this.getUser.bind(this)
+        this.createUser = this.createUser.bind(this)
+        this.updateUser = this.updateUser.bind(this)
+        this.deleteUser = this.deleteUser.bind(this)
+        this.getUserByEmail = this.getUserByEmail.bind(this)
+        this.getUsersByCompanyId = this.getUsersByCompanyId.bind(this)
     }
-    getUser(id: string): User | null {
-        let user = null
-        this.client.user.findUnique({ where: { id } }).then((restult) => {
-            if (!restult) {
-                return
-            }
-            user = restult
-        })
-        return user
+    getUser(id: string): ResultStore<User> {
+        let user
+        try {
+            this.client.user.findUnique({ where: { id } }).then((restult) => {
+                if (!restult) {
+                    return
+                }
+                user = restult
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        if (!user) {
+            return { ok: false, err: { code: StoreErrorCode.notFound, msg: "user not found" } }
+        }
+        return { ok: true, data: user }
     }
 
-    getUsers(): User[] {
+    getUsers(): ResultStore<User[]> {
         let users: User[] = []
-        this.client.user.findMany().then((result) => {
-            if (!result) {
-                return
-            }
-            users = result
-        })
-        return users
+        try {
+            this.client.user.findMany().then((result) => {
+                if (!result) {
+                    return
+                }
+                users = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        return { ok: true, data: users }
     }
 
-    createUser(user: UserCreate): User | null {
+    createUser(user: UserCreate): ResultStore<User> {
         // Check if the user already exists
         let existingUser = null
-        this.client.user.findUnique({ where: { email: user.email } }).then((result) => {
-            if (result) {
-                existingUser = result
-            }
-        })
+        try {
+            this.client.user.findUnique({ where: { email: user.email } }).then((result) => {
+                if (result) {
+                    existingUser = result
+                }
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
         if (existingUser) {
             // throw new Error('User already exists')
-            return null
+            return { ok: false, err: { code: StoreErrorCode.unique, msg: "user already exists" } }
         }
         // Create the user
-        let userResult = null
+        let userResult: User | undefined
         let userPrisma = UserCreatePrismaConverter(user)
-        this.client.user.create({ data: userPrisma }).then((result) => {
-            if (!result) {
-                return
-            }
-            userResult = result
-        })
-        return userResult
+        try {
+            this.client.user.create({ data: userPrisma }).then((result) => {
+                if (!result) {
+                    return
+                }
+                userResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        if (!userResult) {
+            return { ok: false, err: { code: StoreErrorCode.unknown, msg: "internal server error" } }
+        }
+        return { ok: true, data: userResult as User }
     }
 
-    updateUser(id: string, user: Partial<User>): User | null {
-        let userResult = null
-        this.client.user.update({ where: { id }, data: user }).then((result) => {
-            if (!result) {
-                return
-            }
-            userResult = result
-        })
-        return userResult
-
+    updateUser(id: string, user: Partial<User>): ResultStore<User> {
+        let userResult: User | undefined
+        try {
+            this.client.user.update({ where: { id }, data: user }).then((result) => {
+                if (!result) {
+                    return
+                }
+                userResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        if (!userResult) {
+            return { ok: false, err: { code: StoreErrorCode.unique, msg: "user already exists" } }
+        }
+        return { ok: true, data: userResult }
     }
 
-    deleteUser(id: string): User | null {
-        let retulstUser = null
-        this.client.user.delete({ where: { id } }).then((result) => {
-            if (!result) {
-                return
-            }
-            retulstUser = result
-        })
-        return retulstUser
+    deleteUser(id: string): ResultStore<User> {
+        let userResult: User | undefined
+        try {
+            this.client.user.delete({ where: { id } }).then((result) => {
+                if (!result) {
+                    return
+                }
+                userResult = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        if (!userResult) {
+            return { ok: false, err: { code: StoreErrorCode.notFound, msg: "user not found" } }
+        }
+        return { ok: true, data: userResult }
     }
-    getUserByEmail(email: string): User | null {
-        let user = null
-        this.client.user.findUnique({ where: { email } }).then((result) => {
-            if (!result) {
-                return
-            }
-            user = result
-        })
-        return user
+    getUserByEmail(email: string): ResultStore<User> {
+        let user: User | undefined
+        try {
+            this.client.user.findUnique({ where: { email } }).then((result) => {
+                if (!result) {
+                    return
+                }
+                user = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        if (!user) {
+            return { ok: false, err: { code: StoreErrorCode.notFound, msg: "user not found" } }
+        }
+        return { ok: true, data: user }
     }
-    getUsersByCompanyId(companyId: string): User[] {
+    getUsersByCompanyId(companyId: string): ResultStore<User[]> {
         let users: User[] = []
-        this.client.user.findMany({ where: { companyId } }).then((result) => {
-            if (!result) {
-                return
-            }
-            users = result
-        })
-        return users
+        try {
+            this.client.user.findMany({ where: { companyId } }).then((result) => {
+                if (!result) {
+                    return
+                }
+                users = result
+            })
+        } catch (e) {
+            return { ok: false, err: { code: prismaCatchToStoreError(e), msg: "internal server error" } }
+        }
+        return { ok: true, data: users }
     }
 }
