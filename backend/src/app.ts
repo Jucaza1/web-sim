2e9dab908bd7eb1dbeced56428aecdecb78bf374
jucaza1 <juan01cz@gmail.com>
2025-05-06 11:46:58 +0200

    fix: fixed error handler, and json body parser works only relevant methods

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -30,7 +30,14 @@ const corsOptions = {
 
 app.disable('x-powered-by')
 app.use(cookieParser())
-app.use(express.json())
+const jsonParser = express.json()
+app.use((req, res, next) => {
+    if (req.method !== 'GET' && req.method !== 'HEAD' && req.method !== 'OPTIONS') {
+        jsonParser(req, res, next)
+    } else {
+        next()
+    }
+})
 app.use(cors(corsOptions))
 app.get('/health', (_, res: Response) => {
     res.status(200).json({ message: 'Server is OK' })
@@ -73,16 +80,35 @@ app.use("/api/v1", router)
 
 // error handler
 const globalErrorHandler: ErrorRequestHandler = (
-    { httpError, exception }: { httpError: HttpError, exception: Error },
+    // { httpError, exception }: { httpError: HttpError, exception: Error },
+    error: any,
     _req: Request,
     res: Response,
     _next: NextFunction
 ) => {
 
-    console.error(httpError)
-    res.status(httpError.status ?? 500).json({ error: httpError.msg })
-    if (exception) {
-        console.log(exception)
+    if (error.httpError != undefined) {
+        //{ httpError, exception }: { httpError: HttpError, exception: Error } = error
+        console.error(error.httpError as HttpError)
+        res.status(error.httpError.status ?? 500).json({ error: error.httpError.msg })
+        if (error.exception) {
+            console.log(error.exception)
+        }
+        return
+    }
+    if (error.status != undefined) {
+        console.error({ statusCode: error.statusCode, type: error.type })
+        if (error.statusCode === 400) {
+            res.status(error.statusCode as number).json({ error: "bad request" })
+        } else {
+            res.status(error.statusCode as number).json({ error: error.status })
+        }
+        return
+    }
+    if (error instanceof Error) {
+        console.error(error)
+        res.status(500).json({ error: "internal server error" })
+        return
     }
 }
 app.use(globalErrorHandler)
