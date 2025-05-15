e1fb323ff014be794bd95baaff97943e3d4c619d
Byron Panimboza <byron.115@hotmail.com>
2025-05-15 11:41:24 +0200

    feat: implement ProfilePage and ProfileCard components; fetch and display user profile data

---
+++ frontend/src/components/ProfileCard.tsx
@@ -0,0 +1,18 @@
+interface ProfileCardProps {
+    name: string;
+    email: string;
+    profession: string;
+}
+
+const ProfileCard = ({ name, email, profession }: ProfileCardProps) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4">
+      <h1 className="text-2xl font-bold mb-4">Perfil de Usuario</h1>
+        <p className="text-lg font-semibold">Nombre: {name}</p>
+        <p className="text-lg font-semibold">Correo: {email}</p>
+        <p className="text-lg font-semibold">Profesión: {profession}</p>
+    </div>
+  )
+}
+
+export default ProfileCard;
--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,17 +1,68 @@
+import { useEffect, useState } from 'react';
+import ProfileCard from '../components/ProfileCard';
 
+ interface ProfileData {
+        name: string;  
+        email: string;
+        profession: string;
+}
 
 function ProfilePage() {
+   
+    const [profileData, setProfileData] = useState<ProfileData | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
+    useEffect(() => {
+        const fetchProfileData = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/api/v1/dev/users', {
+                    method: 'GET',
+                    //credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json', 
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                    }
+                });
 
+                if (!response.ok) {
+                    throw new Error('Error al recuperar los datos del perfil');
+                }
 
+                const data = await response.json();
+                setProfileData(data);
+            } catch (err) {
+                if (err instanceof Error) {
+                    setError(err.message);
+                } else {
+                    setError('Error desconocido');
+                }
+            } finally {
+                setLoading(false);
+            }
+        }   
+        fetchProfileData();
+    }, []);
 
-
-
-
-
+    if (loading) {
+        return <div>Cargando...</div>;
+    }
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
+    if (!profileData) {
+        return <div>No se encontraron datos de perfil.</div>;
+    }
 
   return (
-    <div className="text-4xl font-bold ">ProfilePage</div>
+    <div className="p-4">
+        <h1 className="text-2xl font-bold mb-4">Perfil de Usuario</h1>
+       <ProfileCard
+        name={profileData.name}
+        email={profileData.email}
+        profession={profileData.profession}
+       />
+    </div>
   )
 }
 
