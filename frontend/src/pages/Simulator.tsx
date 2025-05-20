85fba894e9253284e45c9d643224d153e57725b3
JairoTreBo <trenadobocerojairo@gmail.com>
2025-05-20 17:03:24 +0200

    simulator fetch

--- frontend/src/pages/Simulator.tsx
+++ frontend/src/pages/Simulator.tsx
@@ -1,44 +1,56 @@
-import React, { useEffect } from "react";
-import { Unity, useUnityContext } from "react-unity-webgl";
+import React, { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
-import { simulatorConfigs } from "../components/simulatorsData";
+import { SimulatorWebgl } from "../../../backend/src/types/db";
+import SimulatorUnity from "../components/SimulatorUnity";
+
+const HOST = import.meta.env.VITE_DOMAIN_HOST?? "http://localhost:3000";
+const API_URL = `${HOST}/api/v1`;
 
 const SimulatorApp: React.FC = () => {
+  const [webgl, setWebgl] = useState<SimulatorWebgl>(null);
   const [searchParams] = useSearchParams();
   const id = Number(searchParams.get("id"));
-  const config = simulatorConfigs.get(id);
-
-  if (!config) {
-    return <div>No se encontró configuración para el simulador seleccionado</div>;
-  }
-
-  const { unityProvider, unload } = useUnityContext({
-    loaderUrl: config.loader,
-    dataUrl: config.data,
-    frameworkUrl: config.framework,
-    codeUrl: config.wasm,
-    companyName: "MiEmpresa",
-    productVersion: "1.0",
-    productName: "Simulador",
-  });
-
-useEffect(() => {
-    return () => {
-      unload()
-        .then(() => {
-          console.log("Simulador Unity descargado correctamente");
-        })
-        .catch((e) => {
-          console.error("Error al descargar Unity:", e);
-        });
-    };
-  }, [unload]);
   
-  return (
+
+  useEffect(() => {
+    if (!id) {
+      console.error("Simulador no encontrado");
+      return;
+    }
+      const fetchwebgl = async () => {
+      try {
+        const response = await fetch(`${API_URL}/simulator/${id}/webgl/`,{method:'GET'}); // cambiar la url del back no olvidar
+        if (response.status !== 200) {
+          throw new Error('Error al obtener los simuladores');
+        }
+        const data: SimulatorWebgl = await response.json();
+        setWebgl(data);
+
+      } catch (error) {
+        console.error('Error al obtener simuladores:', error);
+      } finally {
+        //setLoading(false);
+      }
+    };
+
+    fetchwebgl();
+    
+  },[id]); 
+  
+  return(
+  webgl && webgl.loader && webgl.data && webgl.framework && webgl.wasm ? 
     <div className="w-full h-screen">
-      <Unity className="w-full h-full" unityProvider={unityProvider} />
+      <SimulatorUnity props={webgl}/>
+    </div> : 
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold mb-8 text-center">Cargando simulador...</h1>
     </div>
-  );
+  
+  )
+  
+
+
+  
 };
 
 export default SimulatorApp;
